f31af62c263fee3231d741e1f4387ab25e5ab279
EHOTbl <87080886+EHOTbl@users.noreply.github.com>
2021-11-05 22:41:06 +0300

    create new repo react

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+
+
+const Profile = () => {
+  return <div className="content">
+    <div><img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" /></div>
+    { }
+    <div>My posts
+      <div>New post</div>
+      <div>
+        post
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+
+  </div>
+}
+
+export default Profile;
